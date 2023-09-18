f33ee3a6557b9cfa9747d5a3cbcc0187bb6a2dcc
unknown <jjfalcon@uamv.edu.ni>
2023-09-18 15:51:46 -0600

    Primera confirmación en el nuevo repositorio

---
+++ components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+ 
+       <nav>
+        <menu>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        </menu>
+        </nav>  
+  );
+};
+
+export default  Navbar;
---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from "./components/Navbar/Navbar.js"
+const about = () => {
+  return (
+    <div>
+      <Navbar />
+    <p>Esta es la pagina about</p>
+    </div>
+  );
+};
+
+export default about;
